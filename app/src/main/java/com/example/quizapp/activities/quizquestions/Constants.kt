a8e483d1c43df41ee9c1f5019992f18b771c5e8f
stefan <stefanbayne@gmail.com>
2022-11-02 17:12:49 -0400

    First, and final changes.

--- app/src/main/java/com/example/quizapp/activities/quizquestions/Constants.kt
+++ app/src/main/java/com/example/quizapp/activities/quizquestions/Constants.kt
@@ -9,7 +9,7 @@ object Constants{
     const val TOTAL_QUESTIONS: String = "total_questions"
     const val CORRECT_ANSWERS: String = "correct_answers"
 
-    private const val recurringQuestion: String = "What country does this flag belong to?"
+    private const val recurringQuestion: String = "Who is the Star player of this team?"
     // method to return all the questions in an ArrayList of Questions
     // passes in the Question class in the data file Questions.kt
     fun getQuestions(): ArrayList<Question>{
@@ -22,84 +22,84 @@ object Constants{
          * our data class in Question.kt, and return the list.
          * */
 
-        val que1 = Question(1, recurringQuestion, R.drawable.ic_flag_of_argentina,
-            "Argentina",
-            optionTwo = "Armenia",
-            optionThree = "Australia",
-            optionFour = "Armenia",
+        val que1 = Question(1, recurringQuestion, R.drawable.lakers,
+            "Lebron James",
+            optionTwo = "Anthony Davis",
+            optionThree = "Russell Westbrook",
+            optionFour = "Darvin Ham",
             1)
         questionsList.add(que1)
 
 
-        val que2= Question(2, recurringQuestion, R.drawable.ic_flag_of_australia,
-            "Argentina",
-            optionTwo = "Armenia",
-            optionThree = "Australia",
-            optionFour = "Georgia",
+        val que2= Question(2, recurringQuestion, R.drawable.bulls,
+            "Alex Caruso",
+            optionTwo = "Zach Lavine",
+            optionThree = "Demar Derozan",
+            optionFour = "Lonzo Ball",
             3)
         questionsList.add(que2)
 
-        val que3 = Question(3, recurringQuestion, R.drawable.ic_flag_of_belgium,
-            "Canada",
-            optionTwo = "Egypt",
-            optionThree = "USA",
-            optionFour = "Belgium",
+        val que3 = Question(3, recurringQuestion, R.drawable.timberwolves,
+            "Karl Anthony-Towns",
+            optionTwo = "Rudy Gobert",
+            optionThree = "Patrick Beverly",
+            optionFour = "Anthony Edwards",
             4)
         questionsList.add(que3)
 
-        val que4 = Question(4, recurringQuestion, R.drawable.ic_flag_of_brazil,
-            "Brazil",
-            optionTwo = "Spain",
-            optionThree = "New Zealand",
-            optionFour = "Mexico",
+        val que4 = Question(4, recurringQuestion, R.drawable.bucks,
+            "Giannis Antetokounmpo",
+            optionTwo = "Krhis Middleton",
+            optionThree = "Malcolm Brogdon",
+            optionFour = "Jru Holiday",
             1)
         questionsList.add(que4)
 
-        val que5 = Question(5, recurringQuestion, R.drawable.ic_flag_of_denmark,
-            "Canada",
-            optionTwo = "Denmark",
-            optionThree = "Austria",
-            optionFour = "Germany",
+        val que5 = Question(5, recurringQuestion, R.drawable.clippers,
+            "Paul George",
+            optionTwo = "Kawhi Leonard",
+            optionThree = "Luke Kennard",
+            optionFour = "Reggie Jackson",
             2)
         questionsList.add(que5)
 
-        val que6 = Question(6, recurringQuestion, R.drawable.ic_flag_of_fiji,
-            "Fiji",
-            optionTwo = "Armenia",
-            optionThree = "Australia",
-            optionFour = "Belgium",
+        val que6 = Question(6, recurringQuestion, R.drawable.trailblazers,
+            "Damian (Dame Time) Lillard",
+            optionTwo = "Anthony Simons",
+            optionThree = "CJ McCollum",
+            optionFour = "Chauncey Billups",
             1)
         questionsList.add(que6)
 
-        val que7 = Question(7, recurringQuestion, R.drawable.ic_flag_of_germany,
-            "Kuwait",
-            optionTwo = "Germany",
-            optionThree = "New Zealand",
-            optionFour = "Belgium",
+        val que7 = Question(7, recurringQuestion, R.drawable.nets,
+            "Kyrie Irving",
+            optionTwo = "Kevin Durant",
+            optionThree = "Ben Simmons",
+            optionFour = "Seth Curry",
             2)
         questionsList.add(que7)
 
-        val que8 = Question(8, recurringQuestion, R.drawable.ic_flag_of_india,
-            "USA",
-            optionTwo = "Spain",
-            optionThree = "India",
-            optionFour = "Kuwait",
+        val que8 = Question(8, recurringQuestion, R.drawable.hornets,
+            "Gordon Hayward",
+            optionTwo = "Terry Rozier",
+            optionThree = "Lamelo Ball",
+            optionFour = "Kelly Oubre Jr.",
             3)
         questionsList.add(que8)
 
-        val que9 = Question(9, recurringQuestion, R.drawable.ic_flag_of_kuwait,
-            "Argentina",
-            optionTwo = "Kuwait",
-            optionThree = "India",
-            optionFour = "USA",
+        val que9 = Question(9, recurringQuestion, R.drawable.sixers,
+            "Tobias Harris",
+            optionTwo = "James Harden",
+            optionThree = "Tyrese Maxey",
+            optionFour = "Joel Embiid",
             2)
         questionsList.add(que9)
 
-        val que10 = Question(10, recurringQuestion, R.drawable.ic_flag_of_new_zealand,
-            "Argentina",
-            optionTwo = "Germany",
-            optionThree = "France",
-            optionFour = "New Zealand",
+        val que10 = Question(10, recurringQuestion, R.drawable.heat,
+            "Bam Adebayo",
+            optionTwo = "Duncan Robinson",
+            optionThree = "Kyle Lowry",
+            optionFour = "Jimmy Butler",
             4)
         questionsList.add(que10)
         return questionsList
